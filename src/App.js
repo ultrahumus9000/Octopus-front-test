754b6abe34dcb46a44f3fcb989cd19b460d5d1a4
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2021-12-08 15:45:29 +0000

    set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import cart from "./asset/cart.svg";
+import title from "./asset/title.svg";
+import "./App.css";
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={title} />
+        <img className="shopping-cart" src={cart} />
       </header>
     </div>
   );
